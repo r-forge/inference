d03cf1eb7fc6d71bcb18ad0dbbcabe53dec01fb6
nguyenvq (Vinh Nguyen) <vinhdizzo@gmail.com>
2010-10-14 00:26:08 +0000

    S4 set; need @export to work and @import to specify DEPENDS in DESCRIPTION; need to write gee, nlme, lme4, and survival methods
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/inference@4 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/inference.R
+++ pkg/R/inference.R
@@ -8,43 +8,16 @@
 ##' collaboration.
 ##'
 ##' @name inference-package
+##' @rdname inference-package
 ##' @aliases inference
 ##' @docType package
-##' @title Extract inferential information from different statistical
-##' models.
-##' @author Vinh Nguyen \email{vqnguyen@@uci.edu}
-##' @keywords statistical models, point estimates, confidence intervals,
-##' p-values, sample size, inference
+##' @author Vinh Nguyen \email{vinhdizzo at gmail dot com}
+##' @keywords statistical models point estimates confidence intervals
+##' p-values sample size inference
+##' @import methods
 ##' @examples
 ##' infer(lm(rnorm(100) ~ runif(100)))
 NULL
-
-##' <description>
-##'
-##' <details>
-##' @title bye
-##' @param fitobj 
-##' @return 1
-##' @author Vinh Nguyen
-setGeneric("genF", function(fitobj, ...) standardGeneric("genF"))
-##' <description>
-##'
-##' <details>
-##' @title bye
-##' @param fitobj 
-##' @return 1
-##' @author Vinh Nguyen
-setMethod("genF", signature(fitobj="lm"), function(fitobj){ print("lm") })
-##' <description>
-##'
-##' <details>
-##' @title hello
-##' @param fitobj 
-##' @return 1
-##' @author Vinh Nguyen
-setMethod("genF", signature(fitobj="glm"), function(fitobj){ print("glm") })
-
-
 
 ##' An S4 class that stores inferential values of a fitted model object.
 ##'
@@ -53,14 +26,18 @@ setMethod("genF", signature(fitobj="glm"), function(fitobj){ print("glm") })
 ##' estimates (point.est), confidence intervals (ci.lo and ci.hi),
 ##' p-values (p.value), and sample size (n).
 ##'
-##' @name inference-class
-##' @slot .Data matrix consisting of inferential values
-##' @slot sample.size Sample size used in model fit.
+##' @rdname inference-class
+##' @docType class
+##' @slot .Data Object of class \code{matrix}.
+##' @slot model Class of model fit; object of class \code{character},
+##' such as "lm".
+##' @slot sample.size Sample size used in model fit; object of class
+##' \code{numeric}.
 ##' @slot robust.se Boolean indicator whether robust standard errors were
-##' used.
+##' used; object of class \code{logical}.
 ##' @slot two.sided Boolean indicator whether p-values corresond to a
-##' two-sided test or one-sided.
-##' @slot ci.level Confidence level.
+##' two-sided test or one-sided; object of class \code{logical}.
+##' @slot ci.level Confidence level; object of class \code{numeric}.
 setClass(Class="inference"
          , representation=representation(model="character"
              , sample.size="numeric"
@@ -69,43 +46,44 @@ setClass(Class="inference"
              , ci.level="numeric")
          , contains=c("matrix"))
 
-##' Show/print inference object.
+##' Show/print \code{inference} object.
 ##'
-##' show method for objects made using \code{infer()}
-##' @title \code{show} method for \code{inference} class.
-##' @param object 
-##' @return something
+##' \code{show} method for objects made using the \code{infer} function.
+##' @rdname show,inference-method
+##' @aliases print.inference
+##' @param \code{inference} object.
+##' @return Nothing.
 ##' @author Vinh Nguyen
 setMethod("show", "inference", function(object){ print(slot(object, ".Data"))})
 
-##' infer generic function.
-##'
-##' infer() used on fitted model objects, such as \code{lm} objects.
-##' @title infer
-##' @param fitobj 
-##' @param ... 
-##' @return something
-##' @author Vinh Nguyen
-setGeneric("infer", function(fitobj, ...) standardGeneric("infer"))
-
-##' Inference for \code{lm} objects
+##' Inference for fitted model objects.
 ##'
 ##' Extract point estimates, standard errors, confidence intervals,
-##' p-values, and sample size
-##' @title infer-lm
-##' @name infer-methods
-##' @rdname infer-methods
-##' @param fitobj Fitted object.
-##' @param vars Vector of variable names we wish to extract information
-##' for.  Defaults to NULL which corresponds to all variables in the
-##' fitted model.
+##' p-values, and sample size.
+##' @rdname infer,-methods
+##' @aliases infer infer,-method infer,lm-method infer,glm-method
+##' @docType methods
+##' @usage infer(fitobj, vars=NULL, robust.se=TRUE, two.sided=TRUE
+##' , ci.level=0.95)
+##' @param fitobj Fitted model object, such as those of class \code{lm}.
+##' @param vars Vector of variable names to obtain inference information
+##' for.  Defaults to \code{NULL} which corresponds to all variables
+##' in the fitted model.
 ##' @param robust.se Boolean indicator for whether robust standard
-##' errors should be use.  Defaults to TRUE.
+##' errors should be use.  Defaults to \code{TRUE}.
 ##' @param two.sided Boolean indicator for whether p-values should
-##' correspond to a two-sided test or one-sided.  Defaults to TRUE.
+##' correspond to a two-sided test or one-sided.  Defaults to
+##' \code{TRUE}.
 ##' @param ci.level Confidence level.  Defaults to 0.95.
+##' @param ... Not used.
 ##' @return S4 \code{inference} object.
+##' @examples
+##' infer(lm(rnorm(100) ~ runif(100)))
+##' @export
 ##' @author Vinh Nguyen
+setGeneric(name="infer", function(fitobj, ...) standardGeneric("infer"))
+
+##' @nord
 setMethod("infer", signature(fitobj="lm"), function(fitobj, vars=NULL, robust.se=TRUE, two.sided=TRUE, ci.level=0.95)
 {
   if(is.null(vars)) vars <- names(coef(fitobj))
@@ -125,20 +103,8 @@ setMethod("infer", signature(fitobj="lm"), function(fitobj, vars=NULL, robust.se
   rslt <- new("inference", cbind(point.est, se, p.value, ci.lo, ci.hi, n), model=class(fitobj), sample.size=n, robust.se=robust.se, two.sided=two.sided, ci.level=ci.level)
   return(rslt)
 })
-##' <description>
-##'
-##' <details>
-##' @title infer-glm
-##' @name infer-methods
-##' @rdname infer-methods
-##' @param fitobj 
-##' @param vars 
-##' @param robust.se 
-##' @param two.sided 
-##' @param ci.level 
-##' @param transform 
-##' @return S4 \code{inference} object.
-##' @author Vinh Nguyen
+
+##' @nord
 setMethod("infer", signature(fitobj="glm"), function(fitobj, vars=NULL, robust.se=TRUE, two.sided=TRUE, ci.level=0.95, transform=NULL)
 {
   if(is.null(vars)) vars <- names(coef(fitobj))
@@ -162,4 +128,6 @@ setMethod("infer", signature(fitobj="glm"), function(fitobj, vars=NULL, robust.s
 ## library(roxygen)
 ## package.skeleton("inference2", code_files="inference.R", force=TRUE)
 ## roxygenize(package.dir="inference", roxygen.dir="inference", copy.package=FALSE, unlink.target=FALSE)
-## roxygenize(package.dir="inference", roxygen.dir="inference", copy.package=FALSE, unlink.target=FALSE, use.Rd2=TRUE)
+## roxygenize(package.dir="inference", roxygen.dir="inference", copy.package=FALSE, unlink.target=FALSE, use.Rd2=TRUE) ## will try to document all function declarations, even if I did not document it using roxygen
+## roxygenize(package.dir="pkg", roxygen.dir="pkg", copy.package=FALSE, unlink.target=FALSE, use.Rd2=TRUE) ## use this for S4
+## R CMD roxygen -d -s pkg
